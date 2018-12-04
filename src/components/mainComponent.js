7e9c71725c9fe58731638255fb49488764491612
luis Ramirez <l.r.32@hotmail.com>
2018-12-04 13:34:58 -0800

    about us page

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './homeComponent';
+import About from './aboutComponent';
 import Menu from './menuComponent';
 import Contact from './contactComponent';
 import DishDetail from './dishDetailComponent';
@@ -49,6 +50,7 @@ class  Main extends Component {
       <Header />
       <Switch>
         <Route path="/home" component={HomePage} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
         <Route exact path="/menu"component={() => <Menu dishes={this.state.dishes} />} />
         <Route path="/menu/:dishId" component={DishWithId} />
         <Route exact path="/contactus" component={Contact} />
