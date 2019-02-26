a83b8316f37ae065315fbbf709879a0026c37ec5
luis Ramirez <l.r.32@hotmail.com>
2019-02-26 12:28:52 -0800

    fixed nav

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -81,7 +81,7 @@ class Header extends Component {
            <div className="container">
                <div className="row row-header">
                    <div className="col-12 col-sm-6">
-                       <h1>Ristorante con Fusion</h1>
+                       <h1>Luis Ristorante Con Fusion</h1>
                        <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                    </div>
                </div>
