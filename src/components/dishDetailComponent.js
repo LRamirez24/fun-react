9aa25bba2ddbf10883d2b25e9f57e0e0230c6227
luis Ramirez <l.r.32@hotmail.com>
2019-03-13 16:10:47 -0700

    fetch post comments

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -32,7 +32,7 @@ class CommentForm extends Component {
 
       handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
 
@@ -138,7 +138,7 @@ class CommentForm extends Component {
 
 
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
   if (comments != null) {
   return (
     <div>
@@ -153,7 +153,7 @@ function RenderComments({comments, addComment, dishId}) {
             );
           })}
           </ul>
-          <CommentForm dishId={dishId} addComment={addComment} />
+          <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
   }
@@ -213,7 +213,7 @@ function RenderComments({comments, addComment, dishId}) {
                     </div>
                       <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments}
-        addComment={props.addComment}
+        postComment={props.postComment}
         dishId={props.dish.id} />
                         </div>                 
                 </div>
